08aa485eb6ef4869599b81a4db045b85dba741e1
ArthurEnrique15 <arthurenrique1512@gmail.com>
2024-07-09 16:01:09 -0300

    style: update global styles

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,14 +1,31 @@
 'use client'
 
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-  * {
-    padding: 0;
-    margin: 0;
-  }
+  ${({ theme }) => css`
+    * {
+      padding: 0;
+      margin: 0;
 
-  html {
-    height: 100%;
-  }
+      &:focus {
+        outline: none;
+      }
+    }
+
+    html {
+      height: 100%;
+      background-color: ${theme.colors.gray[100]};
+      color: ${theme.colors.gray[800]};
+    }
+
+    body {
+      height: 100%;
+    }
+
+    a {
+      text-decoration: none;
+      color: ${theme.colors.green[300]};
+    }
+  `}
 `
