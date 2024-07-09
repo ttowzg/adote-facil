11fcf0c955d40a827612c382bc409eba3921fd5d
ArthurEnrique15 <arthurenrique1512@gmail.com>
2024-07-09 17:04:49 -0300

    style: ad global input style

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -27,5 +27,19 @@ export const GlobalStyles = createGlobalStyle`
       text-decoration: none;
       color: ${theme.colors.green[300]};
     }
+
+    input {
+      border-radius: 6px;
+
+      color: ${theme.colors.gray[800]};
+      background-color: ${theme.colors.gray[200]};
+      border: 1px solid ${theme.colors.gray[500]};
+
+      padding: 0.5rem 1rem;
+
+      &:focus {
+        border-color: ${theme.colors.green[300]};
+      }
+    }
   `}
 `
