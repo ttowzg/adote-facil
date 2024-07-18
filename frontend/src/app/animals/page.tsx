48c1013cc3a7b5365819d567a6782d2f4f3308dc
ArthurEnrique15 <arthurenrique1512@gmail.com>
2024-07-17 21:42:19 -0300

    refactor: create page animals and redirect to it from home

---
+++ frontend/src/app/animals/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Button } from '@/components/Button'
+import * as S from './styles'
+import { Header } from '@/components/Header'
+import { AnimalCard } from '@/components/AnimalCard'
+import { mockAnimals } from '@/mocks/animals'
+import { EmptyAnimals } from '@/components/EmptyAnimals'
+
+export default function Animals() {
+  return (
+    <S.Wrapper>
+      <Header />
+
+      <S.Content>
+        <S.TitleWrapper>
+          <h1>Animais disponíveis para adoção</h1>
+          {!!mockAnimals.length && <Button>Filtrar</Button>}
+        </S.TitleWrapper>
+        {mockAnimals.length ? (
+          <S.AnimalsListWrapper>
+            {mockAnimals.map((animal) => (
+              <AnimalCard
+                key={animal.id}
+                animal={animal}
+                listType="animals-available-to-adopt"
+              />
+            ))}
+          </S.AnimalsListWrapper>
+        ) : (
+          <EmptyAnimals />
+        )}
+      </S.Content>
+    </S.Wrapper>
+  )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,8 @@
 'use client'
 
-import { Button } from '@/components/Button'
-import * as S from './styles'
-import { Header } from '@/components/Header'
-import { AnimalCard } from '@/components/AnimalCard'
-import { mockAnimals } from '@/mocks/animals'
-import { EmptyAnimals } from '@/components/EmptyAnimals'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <S.Wrapper>
-      <Header />
-
-      <S.Content>
-        <S.TitleWrapper>
-          <h1>Animais disponíveis para adoção</h1>
-          {!!mockAnimals.length && <Button>Filtrar</Button>}
-        </S.TitleWrapper>
-        {mockAnimals.length ? (
-          <S.AnimalsListWrapper>
-            {mockAnimals.map((animal) => (
-              <AnimalCard
-                key={animal.id}
-                animal={animal}
-                listType="animals-available-to-adopt"
-              />
-            ))}
-          </S.AnimalsListWrapper>
-        ) : (
-          <EmptyAnimals />
-        )}
-      </S.Content>
-    </S.Wrapper>
-  )
+  const router = useRouter()
+  router.push('/animals')
 }
