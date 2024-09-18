a9213e20c9932dbed29b38e29b52172bd35f90f2
ArthurEnrique15 <arthurenrique1512@gmail.com>
2024-09-18 19:59:30 -0300

    feat: new login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,43 +1,91 @@
 'use client'
 
-import { FormEvent } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
 import { Button } from '@/components/Button'
 import { PasswordInput } from '@/components/PasswordInput'
 
 import * as S from './styles'
+import logo from '../../assets/logo-big.png'
 
-// TODO validar campos antes do submit e exibir mensagens caso hajam erros
+const createUserFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'O email é obrigatório' })
+    .email('Email inválido'),
+  password: z
+    .string()
+    .min(1, { message: 'A senha é obrigatória' })
+    .min(8, { message: 'A senha deve conter no mínimo 8 caracteres' }),
+})
+
+export type CreateUserFormData = z.infer<typeof createUserFormSchema>
+
+// TODO implementar integracao com o login do backend
 export default function Page() {
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    console.log(event)
+  const router = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  })
+
+  const handleSubmitForm = async (data: CreateUserFormData) => {
+    // const response = await registerUser(data)
+    // if (response.status !== 201) {
+    //   const message =
+    //     response.data.message ||
+    //     'Falha ao cadastrar! Por favor tente novamente!'
+    //   alert(message)
+    //   router.push('/cadastro')
+    //   return
+    // }
+    // alert(
+    //   'Cadastro efetuado com sucesso! Faça login para acessar nossa plataforma!',
+    // )
+    // router.push('/login')
   }
 
   return (
     <S.Wrapper>
       <S.Header>
-        <h1>Login</h1>
+        <Image src={logo} alt="Logo" width={145} height={42} />
       </S.Header>
-
+      <S.Aside>
+        <Image src={logo} alt="Logo" width={145} height={42} />
+      </S.Aside>
       <S.Content>
-        <S.LoginForm onSubmit={handleSubmit}>
+        <S.LoginForm onSubmit={handleSubmit(handleSubmitForm)}>
+          <h1>Faça login em nossa plataforma</h1>
           <S.LoginFormInputsWrapper>
             <label>
               Email
-              <input type="email" />
+              <input type="email" {...register('email')} />
+              {errors.email && <span>{errors.email.message}</span>}
             </label>
             <label>
               Senha
-              <PasswordInput />
+              <PasswordInput zodRegister={register} fieldName="password" />
+              {errors.password && <span>{errors.password.message}</span>}
             </label>
           </S.LoginFormInputsWrapper>
-          <Button type="submit">Login</Button>
+
+          <S.LoginFormButtonWrapper>
+            <Button type="submit">Cadastrar</Button>
+          </S.LoginFormButtonWrapper>
         </S.LoginForm>
-        <footer>
+        <S.LoginFormFooter>
           <span>
-            Ainda não tem uma conta? <a href="/register">Cadastre-se</a>
+            Já possui uma conta? <a href="/login">Faça login</a>
           </span>
-        </footer>
+        </S.LoginFormFooter>
       </S.Content>
     </S.Wrapper>
   )
--- frontend/src/app/login/styles.ts
+++ frontend/src/app/login/styles.ts
@@ -4,24 +4,44 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
+
+  @media (min-width: 770px) {
+    flex-direction: row;
+  }
 `
 
 export const Header = styled.div`
-  ${({ theme }) => css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 1rem;
+
+  height: 3rem;
+
+  @media (min-width: 770px) {
+    display: none;
+  }
+`
+
+export const Aside = styled.aside`
+  display: none;
+
+  @media (min-width: 770px) {
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    align-items: flex-end;
 
-    padding-top: 3rem;
-    padding-bottom: 1rem;
+    padding: 2rem;
 
-    background-color: ${theme.colors.gray[200]};
+    width: 340px;
 
-    h1 {
-      color: ${theme.colors.white};
-      font-size: 1.5rem;
-    }
-  `}
+    background-color: ${({ theme }) => theme.colors.gray[200]};
+  }
+
+  @media (min-width: 1080px) {
+    width: 480px;
+  }
 `
 
 export const Content = styled.div`
@@ -30,39 +50,94 @@ export const Content = styled.div`
   justify-content: space-between;
 
   padding: 1rem;
-  height: 100%;
+  height: 100vh;
 
-  footer {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  @media (min-width: 770px) {
+    max-width: calc(100vw - 340px - 4rem);
+    padding: 0;
+  }
+
+  @media (min-width: 1080px) {
+    max-width: calc(100vw - 480px - 4rem);
   }
 `
 
 export const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
   gap: 1.5rem;
+
+  h1 {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  @media (min-width: 770px) {
+    margin-top: 12rem;
+    padding: 2rem;
+
+    width: calc(100vw - 404px - 4rem);
+  }
+
+  @media (min-width: 1080px) {
+    width: calc(100vw - 544px - 4rem);
+
+    h1 {
+      font-size: 2rem;
+    }
+  }
 `
 
 export const LoginFormInputsWrapper = styled.div`
   ${({ theme }) => css`
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    grid-template-columns: 1fr;
     gap: 1rem;
 
     padding-bottom: 1.5rem;
     border-bottom: 1px solid ${theme.colors.white};
+
+    width: 100%;
+    max-width: 400px;
 
     label {
       display: flex;
       flex-direction: column;
 
       gap: 0.5rem;
+
+      span {
+        font-size: 0.75rem;
+        color: ${theme.colors.red[700]};
+      }
     }
 
     input {
       height: 2rem;
     }
   `}
+`
+
+export const LoginFormButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+
+  button {
+    max-width: 400px;
+  }
+`
+
+export const LoginFormFooter = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 770px) {
+    margin-bottom: 2rem;
+  }
 `
